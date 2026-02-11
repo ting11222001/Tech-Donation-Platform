63dc1d850b9ca590b146013f20b4c89624eb92cb
Li-Ting Liao <39208708+ting11222001@users.noreply.github.com>
2026-02-11 20:59:11 +1030

    Initial commit: Environment setup

---
+++ src/main/java/io/techbridge/donation/TechbridgeApplication.java
@@ -0,0 +1,14 @@
+package io.techbridge.donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbridgeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TechbridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/techbridge/donation/TechbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.techbridge.donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
